811a041441e17badc5ba3854c5356f85608a0c54
sam <zhenyastatham@yandex.ru>
2026-02-11 23:22:09 +0300

    1st commit

---
+++ tcp_client.py
@@ -0,0 +1,19 @@
+import socket 
+
+target_host = "www.google.com"
+target_port = 80
+
+#socket of object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#connecting client
+client.connect((target_host, target_port))
+
+#sending some data
+client.send(b"GET / HTTP/1.1\r\nHOST: google.com\r\n\r\n")
+
+#getting some data
+response = client.recv(4096)
+
+print(response.decode())
+client.close()
